5b92c997ad38d94431b42b3902d285267739cc56
Joao-Victor-Martins <joaovictorferreiramartins02@gmail.com>
2025-04-20 15:43:25 -0300

    implementação do Vercel Analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Footer from "./components/Footer";
 
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <body>
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
